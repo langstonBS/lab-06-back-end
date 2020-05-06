4ac6b7d6c00278a62de1bcb62b57bae3e5abf604
langstonBS <langston.beckwithstanley@gmail.com>
2020-05-05 18:15:25 -0700

    the corse

--- index.js
+++ index.js
@@ -2,23 +2,25 @@ const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
 const cors = require('cors');
-const app = express();
+
 const PORT = process.env.port || 3001;
+const geo = require('./data/geo.json')
 
-
+const app = express();
 app.use(cors());
 
 
 app.get('/location', (req, res) => {
     console.log('HELLO')
     res.json({
-        
-        "formatted_query": "Seattle, WA, USA",
-        "latitude": "47.606210",
-        "longitude": "-122.332071"
+            "formatted_query": "Seattle, WA, USA",
+            "latitude": "47.606210",
+            "longitude": "-122.332071"
         
     });
 });
+
+
 
 
 app.listen(PORT, () => { console.log(`listening on port ${PORT}`); });
